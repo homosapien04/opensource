b1e323122366dc203d66d61414fa69cf5a06433f
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-14 00:18:31 +0530

    Create Season_Game.java

---
+++ visaa_prep/Season_Game.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n==3||n==4||n==5)
+            System.out.println("Spring");
+        else if(n>=6 && n<=8)
+            System.out.println("Summer");
+        else if(n>=9 && n<=11)
+            System.out.println("Autumn");
+        else if(n==12 || n==1 || n==2)
+            System.out.println("Winter");
+        else
+            System.out.println("Invalid");
+    }
+}
