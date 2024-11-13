10a148f6e772695176d538feac2be7e78cf2f27a
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-13 22:56:44 +0530

    Create Test_Score.java

---
+++ visaa_prep/Test_Score.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int x=s.nextInt();
+        int y=s.nextInt();
+        if(y%x==0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
