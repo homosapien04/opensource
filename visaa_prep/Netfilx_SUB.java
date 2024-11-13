535e9df34c243dc85e289a574cf0711e679750e8
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-13 22:44:30 +0530

    Create Netfilx_SUB.java

---
+++ visaa_prep/Netfilx_SUB.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        int x=s.nextInt();
+        if(((a+b)>=x) || ((b+c)>=x) || ((c+a)>=x))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
