5be1371b70d1e7452b807d7f8a2ae9bcaaa8d7a7
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-14 20:57:45 +0530

    Create Reverse_Array.java

---
+++ visaa_prep/Reverse_Array.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
