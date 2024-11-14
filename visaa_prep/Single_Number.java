4fb01e7555b602ff7cc020ff333e9feb3f007b59
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-14 21:57:26 +0530

    Create Single_Number.java

---
+++ visaa_prep/Single_Number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        long ans=a[0];
+        for(int i=1;i<n;i++){
+            ans^=a[i];
+        }
+        System.out.println(ans);
+    }
+}
