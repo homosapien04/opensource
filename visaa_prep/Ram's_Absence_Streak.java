80c8a8c8235ba8c7d8589ad6e49f0fb5d6f214f6
AnuhyaPeddinti <159169792+AnuhyaPeddinti@users.noreply.github.com>
2024-11-16 20:24:18 +0530

    Create Ram's_Absence_Streak.java

---
+++ visaa_prep/Ram's_Absence_Streak.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int c=0;
+        int m=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                c+=1;
+                m=Math.max(c,m);}
+            else
+                c=0;
+        }
+        System.out.println(m);
+    }
+}
