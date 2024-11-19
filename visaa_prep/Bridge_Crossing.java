ec38e2d46aa93b2b892461872207f7e084a2f2bf
homosapien04 <harshithapalle2004@gmail.com>
2024-11-19 11:01:36 +0530

    Create Bridge_Crossing.java

---
+++ visaa_prep/Bridge_Crossing.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int x=s.nextInt();
+        int y=s.nextInt();
+        int z=s.nextInt();
+        int mangoes=(z-y)/x;
+        System.out.println(mangoes);
+    }
+}
